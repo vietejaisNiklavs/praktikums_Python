cdf9af1e778196c88559b31981ab16069eb4e095
Niklavs <niklavs.taurins@grobinasskola.lv>
2021-04-27 09:30:25 +0300

    vainigs

--- uzd4.py
+++ uzd4.py
@@ -1,3 +1,13 @@
 """
 Izveidot funkciju, kura atgriež skaitļu kvadrātus, lietotāja norādītā apgabalā.
 """
+a=int(input("ievadi skaitļu apgabala sākumu: "))
+b=int(input("ievadi skaitļu apgabala beigas: "))
+
+kvadrati = []
+
+for i in range(a, b):
+    kvadrats = i * i
+    kvadrati.append(kvadrats)
+
+print(kvadrati)
